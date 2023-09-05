7214f2820522789704ec49bdc064fcb0be4b3fa3
fangdehui <javaeecoding@gmail.com>
2023-09-05 10:53:21 +0900

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+monorepo by npm workspace
+
+how to run:
+
+1. clone project
+2. cd monorepo
+3. npm i
+4. node module-b/src/index.mjs
---
+++ module-b/src/index.mjs
@@ -0,0 +1,8 @@
+// 导入模块A
+import { name } from 'module-a';
+
+// 模块B代码  
+export const nameB = 'module-b';
+
+console.log(name); // 'module-a'
+console.log(nameB); // 'module-a'
